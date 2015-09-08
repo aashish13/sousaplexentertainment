3a78928d83b2177a1869a1a7121d221e25f31eba
Aashish <aashishkumar.singh@hotmail.com>
2015-09-07 23:44:54 -0400

    compiling only the files required

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
- Rails.application.config.assets.precompile += %w( *.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( bootstrap-responsive.css )
+Rails.application.config.assets.precompile += %w( prettyPhoto.css )
+Rails.application.config.assets.precompile += %w( style.css )
