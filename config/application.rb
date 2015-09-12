a28ca420915e4c23bb17cc2b7695a30661a97ab1
Aashish <aashishkumar.singh@hotmail.com>
2015-09-11 23:30:36 -0400

    assets precompile

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Sousaplexentertainment
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
+
     config.serve_static_assets = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
