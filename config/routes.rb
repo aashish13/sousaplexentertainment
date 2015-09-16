f97e839e69784e8ce7b1f6f75fc66730cc39e621
Aashish <aashishkumar.singh@hotmail.com>
2015-09-15 21:41:43 -0400

    projects page work

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'team' => 'team#index'
   get 'projects' => 'projects#index'
 
+  get '/project/:id', to: 'projects#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
