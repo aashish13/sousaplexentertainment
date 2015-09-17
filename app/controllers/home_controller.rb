2338925517cd1dd2b89a80f9047993f981cdc74e
Aashish <aashishkumar.singh@hotmail.com>
2015-09-17 13:16:51 -0400

    full width menu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    puts "------------------------Aashish Singh------------------------------"
-    puts current_user.admin
     @projects=Project.all
   end
 end
